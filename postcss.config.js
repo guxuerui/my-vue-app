1c050be4ed1012ce9e484bad27690e506b760199
guxuerui <im.guxuerui@gmail.com>
2022-01-14 17:43:05 +0800

    :new: 配置post.config.js，引入autoprefixer插件

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+export const plugins = [
+  // require('postcss-import'),
+  require('tailwindcss'),
+  require('autoprefixer')
+];
