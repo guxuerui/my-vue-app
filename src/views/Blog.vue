e351e733b84d49b0bae3ff9d4e993df12f2335f4
guxuerui <im.guxuerui@gmail.com>
2022-01-14 14:55:31 +0800

    :pencil2: 修复路由拼写错误

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -7,7 +7,7 @@
       <router-link to="/blog/todos" class="px-4 py-2 rounded-lg">Todos</router-link>
     </button>
     <button class="rounded-lg mx-2 my-6 py-2 bg-blue-500 hover:bg-blue-600 text-white">
-      <router-link to="/blog/vmod elTest" class="px-4 py-2 rounded-lg">VmodelTest</router-link>
+      <router-link to="/blog/vmodelTest" class="px-4 py-2 rounded-lg">VmodelTest</router-link>
     </button>
     <button @click="plus">加一</button>
   </div>
