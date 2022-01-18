1c5b35e1537d3b6ca92ccb84a41f07e36db84bdc
guxuerui <im.guxuerui@gmail.com>
2022-01-19 00:16:53 +0800

    :new: 使用ts重构Header.vue代码

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,63 @@
 <template>
   <div id="header" class="w-full flex flex-row justify-center align-center bg-gray-800">
     <div class="text-white font-black mr-12 text-xl my-4 pointer-style" @click="toHome">
-      欢迎来到我的世界 {{ title }}!
+      {{ title }}
     </div>
-    <button class="text-white rounded-lg mx-2 my-4 hover:bg-gray-700">
-      <router-link to="/" class="p-2 font-black rounded-lg">DashBoard</router-link>
-    </button>
-    <button class="text-white rounded-lg mx-2 my-4 hover:bg-gray-700">
-      <router-link to="/team/front" class="p-2 font-black rounded-lg">Team</router-link>
-    </button>
-    <button class="text-white rounded-lg mx-2 my-4 hover:bg-gray-700">
-      <router-link to="/blog" class="p-2 font-black rounded-lg">博客</router-link>
-    </button>
-    <button class="text-white rounded-lg mx-2 my-4 hover:bg-gray-700">
-      <router-link to="/connect" class="p-2 font-black rounded-lg">联系我</router-link>
-    </button>
-    <button class="text-white rounded-lg mx-2 my-4 hover:bg-gray-700">
-      <router-link to="/hello" class="p-2 font-black rounded-lg">你好</router-link>
-    </button>
-    <button class="text-white rounded-lg mx-2 my-4 hover:bg-gray-700">
-      <router-link to="/about" class="p-2 font-black rounded-lg">关于我</router-link>
+    <button
+      class="text-white rounded-lg mx-2 my-4 hover:bg-gray-700"
+      v-for="(item, index) in datas" :key="index"
+    >
+      <router-link :to="item.path" class="p-2 font-black rounded-lg">{{ item.name }}</router-link>
     </button>
   </div>
 </template>
 <script lang="ts" setup>
-  import { ref } from 'vue';
+  import { reactive, toRefs } from 'vue';
   import { useRouter, useRoute } from 'vue-router';
   const router = useRouter();
   const route = useRoute();
   
-  const title = ref ('塞尔达');
+  interface Router {
+    title: string,
+    datas: {
+      [key: string]: any
+    }
+  }
+  const routerData: Router = reactive({
+    title: '下饭达人',
+    datas: [
+      {
+        name: 'DashBoard',
+        path: '/'
+      },
+      {
+        name: 'Team',
+        path: '/team/front'
+      },
+      {
+        name: '博客',
+        path: '/blog'
+      },
+      {
+        name: '联系我',
+        path: '/connect'
+      },
+      {
+        name: '示例',
+        path: '/example'
+      },
+      {
+        name: '关于我',
+        path: '/about'
+      }
+    ]
+  })
   // 回首页
   const toHome = () => {
     router.push('/');
   }
+
+  const { title, datas } = toRefs(routerData);
 </script>
 <style scoped lang="scss">
   #header {
