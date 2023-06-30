fb59a42157d804457569faecc655949bd561580a
guxuerui <im.guxuerui@gmail.com>
2023-06-30 19:02:24 +0800

    ✨ feat(GenericComp.vue): add GenericComp component
    This commit adds a new component called GenericComp.vue. The component uses TypeScript and the Vue Composition API. It defines a generic prop called `T` using the `defineProps` function. The component also includes a function called `setActiveClass` that applies a CSS class based on whether the item is selected or not. The template renders a list of items with the active class applied to the selected item.

---
+++ src/components/GenericComp.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup generic="T">
+import { GenericComp } from '@/types'
+
+const props = defineProps<GenericComp<T>>()
+
+function setActiveClass(item: T) {
+  if (item === props.selected) {
+    return 'text-5xl text-purple-600 font-bold'
+  } else {
+    return ''
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>1. Generics and Importing Types</h1>
+    <ul class="w-1/12 py-4 mx-auto border-2 border-purple-600">
+      <li v-for="(item, idx) in items" :key="idx">
+        <span :class="setActiveClass(item)">item: {{ item }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
