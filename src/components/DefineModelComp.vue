063f8573ed8822a3d586e77ff0b7e86da548b2aa
guxuerui <im.guxuerui@gmail.com>
2023-07-03 14:01:11 +0800

    📦 feat(DefineModelComp.vue): add new component DefineModelComp.vue 🔄 refactor(Vue3.3.vue): import and use DefineModelComp component with v-model directive

---
+++ src/components/DefineModelComp.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const modelValue = defineModel<number>({default: 0})
+</script>
+
+<template>
+  <div>
+    <button
+      @click="modelValue++"
+      class="border-2 border-dashed border-purple-600 px-6 py-3 text-purple-600"
+    >Press Me : {{ modelValue }}</button>
+  </div>
+</template>
--- src/views/Vue3.3.vue
+++ src/views/Vue3.3.vue
@@ -1,14 +1,18 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import GenericComp from '@/components/GenericComp.vue'
+import DefineModelComp from '@/components/DefineModelComp.vue'
+
+const count = ref(0)
+</script>
+
 <template>
   <div class="text-center pt-6 font-bold text-3xl">
     <h1>Test New Vue3.3 API</h1>
     <GenericComp :items="['a', 'b', 'c', 'd']" selected="c" />
+
+    <h1 class="mt-4">2. defimeModel</h1>
+    <h2>defineModel count: {{ count }}</h2>
+    <DefineModelComp v-model="count" />
   </div>
 </template>
-
-<script lang="ts" setup>
-import GenericComp from '@/components/GenericComp.vue'
-</script>
-
-<style lang="scss" scoped>
-
-</style>
